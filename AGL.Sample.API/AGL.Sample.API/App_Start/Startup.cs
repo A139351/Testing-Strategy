f859d94fdf78dcf9b935a16e0ac06be08670367d
Justin Taylor <jtaylor6@agl.com.au>
2017-07-12 16:39:07 +1000

    Got OWIN working

---
+++ AGL.Sample.API/AGL.Sample.API/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using AGL.Sample.API;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace AGL.Sample.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
--- AGL.Sample.API/AGL.Sample.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AGL.Sample.API.WebApiApplication" Language="C#" %>
