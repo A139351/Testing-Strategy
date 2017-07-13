c0b83f9591c59a2a1bf03062a74966103e304cf1
Justin Taylor <jtaylor6@agl.com.au>
2017-07-13 17:44:53 +1000

    Added fluent step library

---
+++ AGL.Sample.API/AGL.Sample.API.BDD.Tests/Features/Numeric/CalculatorController/Fluent/AddTwoNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using AGL.Sample.API.Integration.Tests.Fixtures;
+using AGL.Testing.xUnit;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace AGL.Sample.API.BDD.Tests.Features.Numeric.CalculatorController.Fluent
+{
+    [Collection(FixtureCollections.OwinServerFixtureCollection)]
+    public class AddTests
+    {
+        private readonly OwinServerFixture _serverFixture;
+        private readonly Steps _steps;
+
+        public AddTests(OwinServerFixture serverFixture)
+        {
+            _serverFixture = serverFixture;
+            _steps = new Steps(_serverFixture);
+        }
+
+
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            this.Given(s => _steps.GivenARequestWithTwoNumbers(13, 14))
+                .When(s => _steps.WhenTheRequestExecutes())
+                .Then(_ => _steps.TheResponseCodeIs(HttpStatusCode.OK), "The Response is a success")
+                .And(_ => _steps.TheReturnedContentIs(27))
+                .BDDfy();
+        }
+    }
+}
---
+++ AGL.Sample.API/AGL.Sample.API.BDD.Tests/Features/Numeric/CalculatorController/Fluent/Steps.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http;
+using AGL.Testing.xUnit;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace AGL.Sample.API.BDD.Tests.Features.Numeric.CalculatorController.Fluent
+{
+
+    
+    public class Steps
+    {
+
+
+        private HttpContent _requestContent;
+        private HttpResponseMessage _responseMessage;
+        private readonly OwinServerFixture _serverFixture;
+
+        public Steps(OwinServerFixture serverFixture)
+        {
+            _serverFixture = serverFixture;
+        }
+
+        public void GivenARequestWithTwoNumbers(int number1, int number2)
+        {
+            var request = new[] { number1, number2 };
+            _requestContent = _serverFixture.SerialiseBodyContent(request);
+        }
+
+        public async void WhenTheRequestExecutes()
+        {
+            _responseMessage = await _serverFixture.GetClient<Startup>().PostAsync("api/calculator/add", _requestContent);
+        }
+
+        public void TheResponseCodeIs(HttpStatusCode expectedStatusCode)
+        {
+            Assert.Equal(expectedStatusCode, _responseMessage.StatusCode);
+        }
+
+        public async void TheReturnedContentIs(int expectedResult)
+        {
+            var parsedResponse = int.Parse(await _responseMessage.Content.ReadAsStringAsync());
+            Assert.Equal(expectedResult, parsedResponse);
+        }
+    }
+}
